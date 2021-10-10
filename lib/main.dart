1455a146fdf921c6e2b4cfdd02eb8fe9e37aed48
Ajith V Reji <ajithvreji@gmail.com>
2021-10-10 11:19:33 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MealsApp',
+      title: 'Cafe Bacardi',
       theme: ThemeData(
           primarySwatch: Colors.blue,
           // ignore: deprecated_member_use
